d3d0fdcfb655c5dcd16ad451b0c6375d6e1117a4
Gerald Versluis <gerald.versluis@microsoft.com>
2023-04-07 09:57:49 +0200

    Initial commit

---
+++ MauiCameraMauiBarcodeSample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiCameraMauiBarcodeSample
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MauiCameraMauiBarcodeSample
+ Sample code to demonstrate how to do barcode scanning with the Camera.MAUI plugin in .NET MAUI
